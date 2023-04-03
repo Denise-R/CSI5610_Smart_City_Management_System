85c22a2cf5b5668af07f9e96209923d21fcce618
Denise Rauschendorfer <95630229+Denise-R@users.noreply.github.com>
2023-04-02 21:24:36 -0400

    Add files via upload
    The picture locations/addresses might not match the code after uploading? Also, the home button fails after 1 loop.

---
+++ Group_9/food.py
@@ -0,0 +1,262 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+
+
+from food_Consumption_Management import foodConsumptionManagement
+
+class Ui_FoodManagement(object):
+    def setupUiFood(self, FoodManagement):
+        FoodManagement.setObjectName("FoodManagement")
+        FoodManagement.resize(502, 731)
+        FoodManagement.setWindowIcon(QtGui.QIcon('ou_logo.jpg'))
+        FoodManagement.setStyleSheet("background-color: #123456;")
+
+        self.centralwidget = QtWidgets.QWidget(FoodManagement)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.frame1 = QtWidgets.QFrame(self.centralwidget)
+        self.frame1.setGeometry(QtCore.QRect(19, 19, 461, 661))
+        self.frame1.setStyleSheet("background-color: white;")
+        self.frame1.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame1.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame1.setObjectName("frame1")
+
+        self.titleLabel = QtWidgets.QLabel(self.frame1)
+        self.titleLabel.setGeometry(QtCore.QRect(0, -1, 461, 51))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.titleLabel.setFont(font)
+        self.titleLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.titleLabel.setObjectName("titleLabel")
+
+        self.line = QtWidgets.QFrame(self.frame1)
+        self.line.setGeometry(QtCore.QRect(10, 40, 441, 20))
+        self.line.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.line.setLineWidth(5)
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setObjectName("line")
+
+        self.entryLabel = QtWidgets.QLabel(self.frame1)
+        self.entryLabel.setGeometry(QtCore.QRect(30, 90, 401, 16))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(14)
+        self.entryLabel.setFont(font)
+        self.entryLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.entryLabel.setObjectName("entryLabel")
+
+        self.outputLabel = QtWidgets.QLabel(self.frame1)
+        self.outputLabel.setGeometry(QtCore.QRect(30, 130, 401, 61))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(14)
+        self.outputLabel.setFont(font)
+        self.outputLabel.setFrameShape(QtWidgets.QFrame.Box)
+        self.outputLabel.setStyleSheet("background-color: white; border: 1px solid black;")
+        self.outputLabel.setText("")
+        self.outputLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.outputLabel.setObjectName("outputLabel")
+        FoodManagement.setCentralWidget(self.centralwidget)
+
+        self.button0 = QtWidgets.QPushButton(self.frame1, clicked=lambda: self.enterNums("0"))
+        self.button0.setGeometry(QtCore.QRect(20, 480, 278, 88))
+        self.button0.setStyleSheet("background-color: white; border: 1px solid black;")
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(16)
+        self.button0.setFont(font)
+        self.button0.setObjectName("button0")
+
+        self.button1 = QtWidgets.QPushButton(self.frame1, clicked=lambda: self.enterNums("1"))
+        self.button1.setGeometry(QtCore.QRect(20, 390, 138, 88))
+        self.button1.setStyleSheet("background-color: white; border: 1px solid black;")
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(16)
+        self.button1.setFont(font)
+        self.button1.setObjectName("button1")
+
+        self.button2 = QtWidgets.QPushButton(self.frame1, clicked=lambda: self.enterNums("2"))
+        self.button2.setGeometry(QtCore.QRect(160, 390, 138, 88))
+        self.button2.setStyleSheet("background-color: white; border: 1px solid black;")
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(16)
+        self.button2.setFont(font)
+        self.button2.setObjectName("button2")
+
+        self.button3 = QtWidgets.QPushButton(self.frame1, clicked=lambda: self.enterNums("3"))
+        self.button3.setGeometry(QtCore.QRect(300, 390, 138, 88))
+        self.button3.setStyleSheet("background-color: white; border: 1px solid black;")
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(16)
+        self.button3.setFont(font)
+        self.button3.setObjectName("button3")
+
+        self.button4 = QtWidgets.QPushButton(self.frame1, clicked=lambda: self.enterNums("4"))
+        self.button4.setGeometry(QtCore.QRect(20, 300, 138, 88))
+        self.button4.setStyleSheet("background-color: white; border: 1px solid black;")
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(16)
+        self.button4.setFont(font)
+        self.button4.setObjectName("button4")
+
+        self.button5 = QtWidgets.QPushButton(self.frame1, clicked=lambda: self.enterNums("5"))
+        self.button5.setGeometry(QtCore.QRect(160, 300, 138, 88))
+        self.button5.setStyleSheet("background-color: white; border: 1px solid black;")
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(16)
+        self.button5.setFont(font)
+        self.button5.setObjectName("button5")
+
+        self.button6 = QtWidgets.QPushButton(self.frame1, clicked=lambda: self.enterNums("6"))
+        self.button6.setGeometry(QtCore.QRect(300, 300, 138, 88))
+        self.button6.setStyleSheet("background-color: white; border: 1px solid black;")
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(16)
+        self.button6.setFont(font)
+        self.button6.setObjectName("button6")
+
+        self.button7 = QtWidgets.QPushButton(self.frame1, clicked=lambda: self.enterNums("7"))
+        self.button7.setGeometry(QtCore.QRect(20, 210, 138, 88))
+        self.button7.setStyleSheet("background-color: white; border: 1px solid black;")
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(16)
+        self.button7.setFont(font)
+        self.button7.setObjectName("button7")
+
+        self.button8 = QtWidgets.QPushButton(self.frame1, clicked=lambda: self.enterNums("8"))
+        self.button8.setGeometry(QtCore.QRect(160, 210, 138, 88))
+        self.button8.setStyleSheet("background-color: white; border: 1px solid black;")
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(16)
+        self.button8.setFont(font)
+        self.button8.setObjectName("button8")
+
+        self.button9 = QtWidgets.QPushButton(self.frame1, clicked=lambda: self.enterNums("9"))
+        self.button9.setGeometry(QtCore.QRect(300, 210, 138, 88))
+        self.button9.setStyleSheet("background-color: white; border: 1px solid black;")
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(16)
+        self.button9.setFont(font)
+        self.button9.setObjectName("button9")
+
+        self.buttonClear = QtWidgets.QPushButton(self.frame1, clicked=self.clear)
+        self.buttonClear.setGeometry(QtCore.QRect(300, 480, 138, 88))
+        self.buttonClear.setStyleSheet("background-color: white; border: 1px solid black;")
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(16)
+        self.buttonClear.setFont(font)
+        self.buttonClear.setObjectName("buttonClear")
+
+        self.buttonSubmit = QtWidgets.QPushButton(self.frame1, clicked=self.popup)
+        self.buttonSubmit.setGeometry(QtCore.QRect(70, 590, 320, 50))
+        self.buttonSubmit.setStyleSheet("background-color: white; border: 1px solid black;")
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(16)
+        self.buttonSubmit.setFont(font)
+        self.buttonSubmit.setObjectName("buttonSubmit")
+
+        self.menubar = QtWidgets.QMenuBar(FoodManagement)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 502, 26))
+        self.menubar.setObjectName("menubar")
+        self.menubar.setStyleSheet("background-color: white; color: black;")
+
+        self.menuOptions = QtWidgets.QMenu(self.menubar)
+        self.menuOptions.setObjectName("menuOptions")
+        FoodManagement.setMenuBar(self.menubar)
+
+        self.home = QtWidgets.QAction(FoodManagement)
+        self.home.setObjectName("home")
+        self.help = QtWidgets.QAction(FoodManagement)
+        self.help.setObjectName("help")
+        self.menuOptions.addAction(self.home)
+        self.home.triggered.connect(self.homePressed)
+        self.home.setIcon(QtGui.QIcon('home.png'))
+        self.help.triggered.connect(self.helpPressed)
+        self.help.setIcon(QtGui.QIcon('question.png'))
+        self.menuOptions.addAction(self.help)
+        self.menubar.addAction(self.menuOptions.menuAction())
+
+        self.retranslateUi(FoodManagement)
+        QtCore.QMetaObject.connectSlotsByName(FoodManagement)
+
+
+    def clear(self):
+        self.outputLabel.setText("")
+
+    def enterNums(self, number):
+        current = self.outputLabel.text()
+        self.outputLabel.setText("")
+        self.outputLabel.setText(str(current) + str(number))
+
+    def popup(self):
+        meals = self.outputLabel.text()
+        days = foodConsumptionManagement(int(meals))
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setWindowIcon(QtGui.QIcon('ou_logo.jpg'))
+        msg.setWindowTitle("Food Consumption Info")
+        msg.setText("\nCurrent number of meal(s): " + str(meals) +
+                            "\n\nMinimum number of days to consume meal(s): " + str(days) + "    ")
+        msg.exec_()
+
+    def homePressed(self):
+        import QTDesigner.homePage
+        self.homeWindow = QtWidgets.QMainWindow()
+        self.homeui = QTDesigner.homePage.Ui_MainWindow()
+        self.homeui.setupUiHome(self.homeWindow)
+        #FoodManagement.hide()
+        self.homeWindow.show()
+
+
+    def helpPressed(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setWindowIcon(QtGui.QIcon('question.png'))
+        msg.setWindowTitle("Help: Food Consumption")
+        msg.setText("How to use food Management window described")
+        msg.exec_()
+
+    def retranslateUi(self, FoodManagement):
+        _translate = QtCore.QCoreApplication.translate
+        FoodManagement.setWindowTitle(_translate("FoodManagement", "Food Consumption Management"))
+        self.menuOptions.setTitle(_translate("FoodManagement", "Options"))
+        self.home.setText(_translate("FoodManagement", "Home"))
+        self.help.setText(_translate("FoodManagement", "Help"))
+        self.titleLabel.setText(_translate("FoodManagement", "Food Consumption Management"))
+        self.button7.setText(_translate("FoodManagement", "7"))
+        self.buttonSubmit.setText(_translate("FoodManagement", "Submit"))
+        self.button8.setText(_translate("FoodManagement", "8"))
+        self.button9.setText(_translate("FoodManagement", "9"))
+        self.button5.setText(_translate("FoodManagement", "5"))
+        self.button4.setText(_translate("FoodManagement", "4"))
+        self.button6.setText(_translate("FoodManagement", "6"))
+        self.button3.setText(_translate("FoodManagement", "3"))
+        self.button2.setText(_translate("FoodManagement", "2"))
+        self.button1.setText(_translate("FoodManagement", "1"))
+        self.buttonClear.setText(_translate("FoodManagement", "Clear"))
+        self.button0.setText(_translate("FoodManagement", "0"))
+        self.entryLabel.setText(_translate("FoodManagement", "Enter the number of meals available: "))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    FoodManagement = QtWidgets.QMainWindow()
+    ui = Ui_FoodManagement()
+    ui.setupUiFood(FoodManagement)
+    FoodManagement.show()
+    sys.exit(app.exec_())
---
+++ Group_9/homePage.py
@@ -0,0 +1,145 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+
+class Ui_MainWindow(object):
+    def setupUiHome(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(850, 550)
+        MainWindow.setWindowIcon(QtGui.QIcon('ou_logo.jpg'))
+        MainWindow.setStyleSheet("background-color: #123456;")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.frame1 = QtWidgets.QFrame(self.centralwidget)
+        self.frame1.setStyleSheet("background-color: white;")
+        self.frame1.setGeometry(QtCore.QRect(20, 20, 810, 530))
+        self.frame1.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame1.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame1.setObjectName("frame1")
+
+        self.titleLabel = QtWidgets.QLabel(self.frame1)
+        self.titleLabel.setGeometry(QtCore.QRect(30, 30, 741, 61))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(20)
+        font.setBold(True)
+        self.titleLabel.setFont(font)
+        self.titleLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.titleLabel.setObjectName("titleLabel")
+
+        self.task = QtWidgets.QPushButton(self.frame1)
+        self.task.setGeometry(QtCore.QRect(50, 170, 221, 111))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        font.setBold(True)
+        self.task.setFont(font)
+        self.task.setObjectName("task")
+
+        self.resource = QtWidgets.QPushButton(self.frame1)
+        self.resource.setGeometry(QtCore.QRect(290, 170, 221, 111))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        font.setBold(True)
+        self.resource.setFont(font)
+        self.resource.setObjectName("resource")
+
+        self.transport = QtWidgets.QPushButton(self.frame1)
+        self.transport.setGeometry(QtCore.QRect(530, 170, 221, 111))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        font.setBold(True)
+        self.transport.setFont(font)
+        self.transport.setObjectName("transport")
+
+        self.booking = QtWidgets.QPushButton(self.frame1)
+        self.booking.setGeometry(QtCore.QRect(50, 320, 221, 111))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        font.setBold(True)
+        self.booking.setFont(font)
+        self.booking.setObjectName("booking")
+
+        self.food = QtWidgets.QPushButton(self.frame1, clicked=self.food)
+        self.food.setGeometry(QtCore.QRect(290, 320, 221, 111))
+        self.food.setStyleSheet("background-image : url(foodImage.png);")
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        font.setBold(True)
+        self.food.setFont(font)
+        self.food.setObjectName("food")
+
+        self.emergency = QtWidgets.QPushButton(self.frame1)
+        self.emergency.setGeometry(QtCore.QRect(530, 320, 221, 111))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        font.setBold(True)
+        self.emergency.setFont(font)
+        self.emergency.setObjectName("emergency")
+
+        self.line = QtWidgets.QFrame(self.frame1)
+        self.line.setGeometry(QtCore.QRect(90, 80, 621, 20))
+        self.line.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.line.setLineWidth(5)
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setObjectName("line")
+
+        self.namesLabel = QtWidgets.QLabel(self.frame1)
+        self.namesLabel.setGeometry(QtCore.QRect(30, 460, 741, 61))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(8)
+        self.namesLabel.setFont(font)
+        self.namesLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.namesLabel.setObjectName("namesLabel")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+
+    def food(self):
+        import QTDesigner.food
+        self.foodWindow = QtWidgets.QMainWindow()
+        self.foodUI = QTDesigner.food.Ui_FoodManagement()
+        self.foodUI.setupUiFood(self.foodWindow)
+        MainWindow.hide()
+        self.foodWindow.show()
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Smart City Managment System"))
+        self.titleLabel.setText(_translate("MainWindow", "Smart City Management System"))
+        self.task.setText(_translate("MainWindow", "Task Scheduler"))
+        self.resource.setText(_translate("MainWindow", "Resource Allocation"))
+        self.transport.setText(_translate("MainWindow", "Transportation"))
+        self.booking.setText(_translate("MainWindow", "Resource Booking"))
+        self.food.setText(_translate("MainWindow", "Food Consumption"))
+        self.emergency.setText(_translate("MainWindow", "Emergency Services"))
+        self.namesLabel.setText(_translate("MainWindow", "Created by: Viljo Wagner, Asad Vakil, Stephen Villagonzalo, "
+                                                         "Jurnee Tipton, Denise Rauschendorfer, & Irma Rrushi "))
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUiHome(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+
