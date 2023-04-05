a0894a46b1ca3768cf5992928e843086551ee98a
Stephen <svillagonzalo@Stephens-MBP.localdomain>
2023-04-05 00:43:01 -0400

    playing with OOP and classes

--- Stephen_Upload/qtTestUI.py
+++ Stephen_Upload/qtTestUI.py
@@ -1,3 +1,4 @@
+import secrets
 from PySide2 import QtWidgets
 
 import ShortestRoute
@@ -6,6 +7,28 @@ class MyQtApp(ShortestRoute.Ui_MainWindow, QtWidgets.QMainWindow):
     def __init__(self):
         super(MyQtApp, self).__init__()
         self.setupUi(self)
+        self.setWindowTitle("Transportation Management")
+    #    self.populate_table_widget()
+    
+    def populate_table_widget(self):
+        self.tableWidget.clear()
+      #  x = ["apple","banana","mango","orange"]
+      # for e in x:
+      #    item = QtWidgets.QTableWidgetItem() 
+      #   item.setText(e)
+
+    def addRow(self):
+    # Retrieve text from QLineEdit
+        x = self.x_input.text()
+        y = self.y_input.text()
+        z = self.z_input.text()
+        # Create a empty row at bottom of table
+        numRows = self.tableWidget.rowCount()
+        self.tableWidget.insertRow(numRows)
+        # Add text to the row
+        self.tableWidget.setItem(numRows, 0, QtWidgets.QTableWidgetItem(x))
+        self.tableWidget.setItem(numRows, 1, QtWidgets.QTableWidgetItem(y))
+        self.tableWidget.setItem(numRows, 2, QtWidgets.QTableWidgetItem(z))
 
 
 if __name__ == '__main__':
@@ -13,11 +36,3 @@ if __name__ == '__main__':
     qt_app = MyQtApp()
     qt_app.show()
     app.exec_()
-
-"""
-if __name__ == "__main__":
-    app = QApplication([])
-    window = MainWindow()
-    window.show()
-    app.exec_()
-"""
